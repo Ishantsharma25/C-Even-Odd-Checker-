710a9327b051fa88bcd51659378490f0e92c83e3
Ishantsharma25 <ishantsharma2027@gmail.com>
2026-09-11 14:36:21 +0530

    Add even odd checker

---
+++ Even,Odd Checker.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int  value;
+    printf("Enter the number:");
+    scanf("%d",&value);
+   if(value % 2==0)
+      { printf("Number is EVEN.");}
+    else 
+
+         { printf("Number is ODD.");}
+       
+    return 0;}
